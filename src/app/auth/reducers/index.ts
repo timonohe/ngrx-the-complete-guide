aaccf1c9c052877ade35fa264a10bcc8bdb47226
Timo Nohe <timo.ademeit@gmx.de>
2022-04-29 12:43:29 +0200

    implemented auth reducer

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -1,11 +1,21 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
+import { AuthActions } from '../action-types';
+import { User } from '../model/user.model';
 
 export const authFeatureKey = 'auth';
 
-export interface State {
-
+export interface AuthState {
+  user: User
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const initialAuthState: AuthState = {
+  user: undefined
+}
 
-};
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => ({
+    ...state,
+    user: action.user
+  }))
+);
