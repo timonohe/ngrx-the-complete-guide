a00b0baf2436396a504228b0132cb2eca360fc2f
Timo Nohe <timo.ademeit@gmx.de>
2022-05-17 12:38:50 +0200

    44. NgRx Data CRUD - Why Pessimistic Data Creation?

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -66,6 +66,13 @@ export class EditCourseDialogComponent {
     if (this.mode === 'update') {
       this.coursesService.update(course);
       this.dialogRef.close();
+    } else if (this.mode === 'create') {
+      this.coursesService.add(course).subscribe(
+        newCourse => {
+          console.log("new Course", newCourse);
+          this.dialogRef.close();
+        }
+      );
     }
   }
 
